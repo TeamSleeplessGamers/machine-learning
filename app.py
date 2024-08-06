b855090c5a1da8ffe14782b0a747b674deeb3bae
Montrell <jubileeinvestmentsdmv@gmail.com>
2024-08-05 23:52:16 -0400

    update call waitress

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from app_factory import create_app
+import waitress
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use waitress to serve the app
+    waitress.serve(app, host='0.0.0.0', port=8000)
