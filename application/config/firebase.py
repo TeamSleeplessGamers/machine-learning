cc1cae5ae8d201685ee35b13b0e7d02f904ef4d5
Montrell <jubileeinvestmentsdmv@gmail.com>
2024-08-07 16:10:20 -0400

    update path

--- application/config/firebase.py
+++ application/config/firebase.py
@@ -6,11 +6,9 @@ import os
 def initialize_firebase():
     # Initialize Firebase
     if not firebase_admin._apps:
-        print("lets start", os.environ['FIREBASE_CRED_PATH'])
         cred  = credentials.Certificate(os.environ['FIREBASE_CRED_PATH'])
         firebase_admin.initialize_app(cred, {
             'databaseURL': os.environ['FIREBASE_DATABASE_URL']
         })
-
 def get_database():
     return db.reference()
