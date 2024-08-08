e9e0c923a8a503805409fb58e79f01be12547d75
Montrell <jubileeinvestmentsdmv@gmail.com>
2024-08-08 16:05:35 -0400

    remove file

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-
-load_dotenv(".env")
-
-from application import create_app, app
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(host='0.0.0.0', port=8000)
