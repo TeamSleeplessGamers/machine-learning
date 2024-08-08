07c9be67c0c1d30ed0e81c3a867fc0d694f6a26b
Montrell <jubileeinvestmentsdmv@gmail.com>
2024-08-08 19:56:34 -0400

    reming print stmt

--- application/__init__.py
+++ application/__init__.py
@@ -3,16 +3,20 @@ from flask_cors import CORS
 from .config.firebase import initialize_firebase
 from .routes.routes import routes_bp
 
-app = Flask(__name__)
-
 def create_app():
-    """Factory to create the Flask application
-    :return: A `Flask` application instance
+    """Factory to create the Flask application.
+    
+    :return: A `Flask` application instance.
     """
-    initialize_firebase()
+    app = Flask(__name__)
+
+    try:
+        initialize_firebase()
+    except Exception as e:
+        app.logger.error(f"Error initializing Firebase: {e}")
+
     CORS(app, origins='*')
 
-    # Register the Blueprint
     app.register_blueprint(routes_bp)
 
     return app
