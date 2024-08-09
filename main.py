c943e366cc2e89aceacc367f957850604913fbd9
Montrell <jubileeinvestmentsdmv@gmail.com>
2024-08-08 20:06:32 -0400

    remove main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from application import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    print("Starting Flask application...")
-    app.run(debug=True, host='0.0.0.0', port=8000)
