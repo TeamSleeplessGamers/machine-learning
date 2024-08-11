fb9a04218e78b10d66f50a92b78f1eb276d67fe1
Montrell <jubileeinvestmentsdmv@gmail.com>
2024-08-10 21:14:23 -0400

    updating lets see if i get numbers

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from .config.firebase import initialize_firebase
+import numpy as np
 from .routes.routes import routes_bp
 import cv2
 
@@ -12,39 +13,35 @@ def create_app():
     app = Flask(__name__)
 
     try:
-        # initialize_firebase()
+        initialize_firebase()
         image_path = '/Users/trell/Projects/machine-learning/frames/output_gray_frame_2400.jpg'
         frame = cv2.imread(image_path)
         
         if frame is None:
             raise ValueError(f"Failed to load image at {image_path}")
 
-        # Convert the image to grayscale
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         thresh_frame = cv2.threshold(gray_frame, 0, 255,
 	        cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-        output = cv2.connectedComponentsWithStats(thresh_frame, 4, cv2.CV_32S)
-        (numLabels, labels, stats, centroids) = output
+        color_frame = cv2.cvtColor(thresh_frame, cv2.COLOR_GRAY2BGR)
+        
+        rectangles = [
+            (1798, 45, 60, 30),
+            (25, 300, 50,50),
+        ]
+        output_dir = '/Users/trell/Projects/machine-learning/frames_processed'
 
-        for i in range(0, numLabels):
-            if i == 0:
-                text = "examining component {}/{} (background)".format(
-                    i + 1, numLabels)
-            else:
-                text = "examining component {}/{}".format( i + 1, numLabels)
-            print("[INFO] {}".format(text))
-            x = stats[i, cv2.CC_STAT_LEFT]
-            y = stats[i, cv2.CC_STAT_TOP]
-            w = stats[i, cv2.CC_STAT_WIDTH]
-            h = stats[i, cv2.CC_STAT_HEIGHT]
-            area = stats[i, cv2.CC_STAT_AREA]
-            (cX, cY) = centroids[i]
+        for i, (x, y, w, h) in enumerate(rectangles):
+            roi = color_frame[y:y + h, x:x + w]
+            
+            output_path = f'{output_dir}/extracted_region_{i + 1}.jpg'
+            cv2.imwrite(output_path, roi)
+            print(f"Extracted region saved to {output_path}")
 
-        output = frame.copy()
-        componentMask = (labels == i).astype("uint8") * 255
-        cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 3)
-        cv2.circle(output, (int(cX), int(cY)), 4, (0, 0, 255), -1)
-        cv2.imwrite('/Users/trell/Projects/machine-learning/frames_processed/processed_gray_frame_2400.jpg', componentMask)
+
+        for (x, y, w, h) in rectangles:
+            img_with_box = cv2.rectangle(color_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.imwrite('/Users/trell/Projects/machine-learning/frames_processed/processed_gray_frame_2400.jpg', img_with_box)
     except Exception as e:
         app.logger.error(f"Error initializing Firebase: {e}")
 
