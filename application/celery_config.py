d4e3a711de19af2dc634c24ff776b21d73dd3d09
Montrell <mjubilee@leaptodigital.com>
2025-06-13 19:53:24 -0400

    add ssl

--- application/celery_config.py
+++ application/celery_config.py
@@ -1,20 +1,25 @@
 import os
+import ssl
 from celery import Celery
 from dotenv import load_dotenv
 
 load_dotenv()
 
+redis_url = os.environ.get("REDIS_URL")
+
 celery = Celery(
     "application",
-    broker=os.environ.get("REDIS_URL"),
-    backend=os.environ.get("REDIS_URL")
+    broker=redis_url,
+    backend=redis_url
 )
 
 celery.conf.update(
     task_serializer='json',
     result_serializer='json',
     accept_content=['json'],
+    redis_backend_use_ssl={
+        "ssl_cert_reqs": ssl.CERT_REQUIRED
+    }
 )
 
-# Force import to ensure task is registered
 from application.tasks import stream_task
